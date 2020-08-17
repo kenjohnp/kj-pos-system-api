5190a31456ae5618e284fb47506d061a20cb5697
Kenjohn Pacana <kenjohnp@gmail.com>
2020-08-17 19:56:24 +0800

    Done adding stock upon stock entry

--- routes/stockEntries.js
+++ routes/stockEntries.js
@@ -1,7 +1,9 @@
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const validate = require("../middleware/validate");
+const validateObjectId = require("../middleware/validateObjectId");
 const { StockEntry, validateStockEntry } = require("../models/stockEntry");
+const { Product } = require("../models/product");
 const express = require("express");
 const router = express.Router();
 
@@ -19,8 +21,33 @@ router.post(
 
     await stockEntry.save();
 
+    const products = req.body.items.map(
+      async (i) =>
+        await Product.findByIdAndUpdate(
+          { _id: i.productId },
+          {
+            $inc: {
+              inStock: i.qty,
+            },
+          },
+          {
+            new: true,
+          }
+        )
+    );
+
     res.send(stockEntry);
   }
 );
+1;
+router.get("/:id", [auth, validateObjectId], async (req, res) => {
+  const stockEntry = await StockEntry.findOne({ _id: req.params.id });
+
+  if (!stockEntry)
+    return res
+      .status(404)
+      .send("The stock entry with the given id was not found.");
+  res.send(stockEntry);
+});
 
 module.exports = router;
