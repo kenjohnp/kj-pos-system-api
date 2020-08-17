775a7028677c0a477f61dfc3355b076cab9429e5
Kenjohn Pacana <kenjohnp@gmail.com>
2020-08-18 01:12:23 +0800

    Added cancel stock entry

--- routes/stockEntries.js
+++ routes/stockEntries.js
@@ -17,17 +17,19 @@ router.post(
   "/",
   [auth, admin, validate(validateStockEntry)],
   async (req, res) => {
-    const { supplier, date, refNo, remarks } = req.body;
+    const { supplier, date, refNo, remarks, stockInBy, items } = req.body;
 
     let stockEntry = {
       supplier: {
         _id: supplier._id,
         name: supplier.name,
       },
+      stockInBy,
       date,
       refNo,
       remarks,
       status: "Added to Stock",
+      items,
     };
 
     stockEntry = new StockEntry(stockEntry);
@@ -49,10 +51,43 @@ router.post(
         )
     );
 
+    stockEntry.items = products;
+
     res.send(stockEntry);
   }
 );
-1;
+
+router.put("/:id", [auth, validateObjectId], async (req, res) => {
+  const stockEntry = await StockEntry.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      status: "Cancelled",
+    },
+    { new: true }
+  );
+
+  await stockEntry.save();
+
+  console.log(stockEntry);
+
+  const productsToBeCancelled = stockEntry.items.map(
+    async (i) =>
+      await Product.findByIdAndUpdate(
+        { _id: i.productId },
+        {
+          $inc: {
+            inStock: -i.qty,
+          },
+        },
+        { new: true }
+      )
+  );
+
+  stockEntry.items = productsToBeCancelled;
+
+  res.send(stockEntry);
+});
+
 router.get("/:id", [auth, validateObjectId], async (req, res) => {
   const stockEntry = await StockEntry.findOne({ _id: req.params.id });
 
